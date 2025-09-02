046140adf80b97588d14a369f6645b82e0f08453
horquidia-lima <horquidia.a.lima@gmail.com>
2025-09-02 14:22:22 -0300

    Criando Componente Icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import TrashIcon from "./assets/icons/trash.svg?react"
+import Icon from "./components/icon"
+
 export default function App() {
 
   return (
-    <>
-      <h1>Oi</h1>
-    </>
+    <div className="felx gap-1">
+      <Icon svg={TrashIcon} className="fill-pink-base"/>
+    </div>
   )
 }
 
