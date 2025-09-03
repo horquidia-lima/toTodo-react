a6c9309190e5089690d78e26967b8d7df766d0d3
horquidia-lima <horquidia.a.lima@gmail.com>
2025-09-03 17:00:30 -0300

    <Badge variant=secondary>5</Badge>

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import TrashIcon from "./assets/icons/trash.svg?react"
+import Badge from "./components/badge"
 import Icon from "./components/icon"
 
 export default function App() {
@@ -6,6 +7,9 @@ export default function App() {
   return (
     <div className="felx gap-1">
       <Icon svg={TrashIcon} className="fill-pink-base"/>
+
+      <Badge variant="secondary">5</Badge>
+      <Badge variant="primary">2 de 5</Badge>
     </div>
   )
 }
