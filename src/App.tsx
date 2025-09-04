7e0a2a7a3b92f80d842968523657374cb0352ea3
horquidia-lima <horquidia.a.lima@gmail.com>
2025-09-04 20:13:24 -0300

    Criando Componente Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import TrashIcon from "./assets/icons/trash.svg?react"
+import PlusIcon from "./assets/icons/plus.svg?react"
 import Badge from "./components/badge"
 import Icon from "./components/icon"
+import Button from "./components/button"
 
 export default function App() {
 
@@ -10,6 +12,10 @@ export default function App() {
 
       <Badge variant="secondary">5</Badge>
       <Badge variant="primary">2 de 5</Badge>
+
+      <div>
+        <Button icon={PlusIcon}>Nova tarefa</Button>
+      </div>
     </div>
   )
 }
